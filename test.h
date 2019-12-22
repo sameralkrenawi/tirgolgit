a18af2c173ebe6f7f2896e2115778e022b5e3133
DanKrazel <dankr2@ac.sce.ac.il>
2019-12-22 18:33:07 +0200

    Update test.h

--- test.h
+++ test.h
@@ -29,3 +29,23 @@ private:
         }
     }
 };
+
+class Student :Person {
+
+private:
+	int average;
+	string institute;
+
+	Student::Student(int average, string institute)
+	{
+		this.average = average;
+		if (institute.length < 10)
+			this.institute = institute;
+		else {
+			throw std::invalid_argument("Invalid length of institute");
+		}
+
+
+	}
+
+};
