3d8442ddb71e989397e41d3e48a2544f09058aaf
DavidTe2 <58590989+DavidTe2@users.noreply.github.com>
2019-12-22 21:02:59 +0200

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -7,7 +7,7 @@ Employee::Employee(){
 }
 
 Employee::Employee(const Employee& other):Person(other)// copy of the person (heritance)
-{years=other.years;// just copy of the only variable of the class}
+{salary=other.salary;// just copy of the only variable of the class}
 
 Employee ::Employee (string pname, long id, int page,float salary):Person(pname,id,page)
     {
