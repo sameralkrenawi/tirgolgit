ace971edef08af87b84a882a7c20fb6f1f39f163
Eytan <58141479+Eytan12@users.noreply.github.com>
2019-12-22 20:00:12 +0200

    Update WorkingStudent.h

--- WorkingStudent.h
+++ WorkingStudent.h
@@ -1,19 +1,13 @@
-
 #include <iostream>
 #include <stdexcept>
 
 using namespace std;
 
-class WorkingStudent:public Student:public Employee {
+class WorkingStudent:public Student :public Employee {
 private:
     bool same_institue=true;
 public:
         WorkingStudent();
         WorkingStudent(bool same_institue);
         ~WorkingStudent();
-
-    WorkingStudent:WorkingStudent(bool same_institue)
-    {
-        this.same_institue=same_institue;
-    }
 };
